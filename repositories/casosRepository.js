4d8ef46eed6d07b4420cf933b12b2cc9d7c0b48c
Silas <silasrviana@gmail.com>
2025-07-23 13:15:48 -0300

    Endpoints GET /casos feitos e estruturação dos outros

--- repositories/casosRepository.js
+++ repositories/casosRepository.js
@@ -1,18 +1,35 @@
 const casos = [
     {
         id: "f5fb2ad5-22a8-4cb4-90f2-8733517a0d46",
-        titulo: "homicidio",
+        titulo: "Homicidio",
         descricao: "Disparos foram reportados às 22:33 do dia 10/07/2007 na região do bairro União, resultando na morte da vítima, um homem de 45 anos.",
         status: "aberto",
-        agente_id: "401bccf5-cf9e-489d-8412-446cd169a0f1" 
-    
+        agente_id: "401bccf5-cf9e-489d-8412-446cd169a0f1"
+
+    },
+    {
+        id: "a3e913a7-52b3-41df-a26f-3f9f6eafbd7a",
+        titulo: "Roubo a mão armada",
+        descricao: "Uma padaria foi assaltada às 18:10 do dia 05/06/2012, no bairro Santo Antônio. Dois suspeitos armados fugiram em uma moto preta.",
+        status: "solucionado",
+        agente_id: "401bccf5-cf9e-489d-8412-446cd169a0f1"
+    },
+    {
+        id: "b97d2dc4-70cf-46c7-9c6a-78e981749fba",
+        titulo: "Furto de veículo",
+        descricao: "Um carro modelo Fiat Palio, cor vermelha, foi furtado na madrugada de 14/09/2019 no estacionamento do shopping central.",
+        status: "aberto",
+        agente_id: "401bccf5-cf9e-489d-8412-446cd169a0f1"
     }
-    //Demais objetos
 ]
 
 function findAll() {
     return casos
 }
+function findById(id) {
+    return casos.find(caso => caso.id === id);
+}
 module.exports = {
-    findAll
+    findAll,
+    findById
 }
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const casosRepository = require('./repositories/casosRepository')
+const agentesRepository = require('./repositories/agentesRepository')
 const app = express();
 const PORT = 3000;
 
@@ -24,59 +26,115 @@ app.listen(PORT, () => {
 -----------------Rotas raiz-------------------
 --------------------------------------------*/
 app.get('/', (req, res) => {
-    res.send('Servidor do Departamento de Polícia rodando em localhost:3000');
+    res.status(200).send('Servidor do Departamento de Polícia rodando em localhost:3000');
 });
 
 /*--------------------------------------------
 ---------------Rotas dos casos----------------
 --------------------------------------------*/
 app.post('/casos', (req, res) => {
-    
+    res.status(201);
 });
 
 app.delete('/casos/:id', (req, res) => {
-    
+    const id = req.params.id;
+    res.status(204);
 });
 
 app.get('/casos', (req, res) => {
-    
+    const casos = casosRepository.findAll();
+
+    let html = `
+        <html>
+        <head>
+            <title>Lista de Casos</title>
+        </head>
+        <body>
+            <h1>Casos Registrados</h1>
+            <div style="border: 1px solid black; border-radius: 5px; padding: 10px">
+    `;
+    casos.forEach(caso => {
+        html += `
+            <div style="border: 1px solid black; border-radius: 5px; padding: 10px">
+                <h2><strong>${caso.titulo}</strong></h2>
+                <p><strong>${caso.status}</strong></p>
+                <p>${caso.descricao}</p>
+                <p>Agente responsável: ${caso.agente_id}</p>
+            </div>
+            <br>
+        `;
+    });
+    html += `
+            </div>
+        </body>
+        </html>
+    `;
+    res.status(200).send(html);
 });
 
 app.get('/casos/:id', (req, res) => {
-    
+    const id = req.params.id;
+    const caso = casosRepository.findById(id);
+
+    let html;
+
+    if (!caso) {
+        return res.status(404).send("<p>Caso não encontrado<p>");
+    } else {
+        html = `
+        <html>
+        <head>
+            <title>Lista de Casos</title>
+        </head>
+        <body>
+            <h1>Casos Encontrados</h1>
+            <div style="border: 1px solid black; border-radius: 5px; padding: 10px">
+                <h2><strong>${caso.titulo}</strong></h2>
+                <p><strong>${caso.status}</strong></p>
+                <p>${caso.descricao}</p>
+                <p>Agente responsável: ${caso.agente_id}</p>
+            </div>
+        </body>
+        </html>
+        `
+    }
+
+    res.status(200).send(html);
 });
 
 app.put('/casos/:id', (req, res) => {
-    
+    const id = req.params.id;
+    res.status(200);
 });
 
 app.patch('/casos/:id', (req, res) => {
-    
+    const id = req.params.id;
+    res.status(200);
 });
 
 /*--------------------------------------------
 --------------Rotas dos agentes---------------
 --------------------------------------------*/
 app.post('/agentes', (req, res) => {
-    
+    res.status(201);
 });
 
 app.delete('/agentes/:id', (req, res) => {
-    
+    res.status(204);
 });
 
 app.get('/agentes', (req, res) => {
-    
+    res.status(200);
 });
 
 app.get('/agentes/:id', (req, res) => {
-    
+    res.status(200);
 });
 
 app.put('/agentes/:id', (req, res) => {
-    
+    res.status(200);
 });
 
 app.patch('/agentes/:id', (req, res) => {
-    
+    res.status(200);
 });
